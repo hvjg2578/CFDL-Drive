fb8739d92cb19db64a1211551deb74bb8f93b5c6
hvjg2578 <47745620+hvjg2578@users.noreply.github.com>
2021-08-16 23:48:01 +0800

    更改表格的css样式

--- header.php
+++ header.php
@@ -14,6 +14,108 @@
 	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
 	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@1.0.1/dist/css/mdui.min.css"/>
+	<style>
+	
+.footer-V{
+    background: #333;
+    flex: 0 0 auto;
+}
+	    .foot-container_2X1Nt {
+	       /*width: 100%;*/
+    height:100px;   /* footer的高度一定要是固定值*/ 
+    /*position:absolute;*/
+    bottom:0px;
+    text-align: left;
+    height: 42px;
+    line-height: 42px;
+    border-top: none;
+    margin-top: 0;
+    background: #f5f6f5;
+}
+* {
+  box-sizing: border-box;
+}
+
+.subbox table {
+  /*max-width: 800px;*/
+  margin: 40px auto;
+  text-align: left;
+  border-spacing: 0;
+  color: #333;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  overflow: hidden;
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.1);
+}
+
+.subbox td {
+  padding: 8px 16px;
+  font-size: 14px;
+  border-bottom: 1px solid #f4f4f4;
+}
+
+.subbox th {
+  padding: 16px;
+}
+
+.subbox img {
+  width: 40px;
+  border-radius: 50%;
+}
+
+.subbox tr:last-child > td {
+  border: 0;
+}
+
+.subbox tbody > tr:hover {
+  background-color: rgba(221, 221, 221, 0.2);
+}
+
+.subbox thead {
+  text-transform: uppercase;
+  font-size: 12px;
+  background-color: #efefef;
+  letter-spacing: 0.5px;
+  color: rgba(0, 0, 0, 0.4);
+}
+
+.option {
+  display: inline-block;
+  padding: 5px 10px;
+  background-color: #ddd;
+  border-radius: 4px;
+  margin-right: 15px;
+}
+
+.name {
+  min-width: 110px;
+  display: inline-block;
+}
+
+.comment {
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 300px;
+  display: inline-block;
+}
+
+.option.is-blue {
+  background-color: #bceefd;
+}
+
+.option.is-orange {
+  background-color: #ffd89e;
+}
+
+.option.is-purple {
+  background-color: #e9cbff;
+}
+
+.option.is-green {
+  background-color: #bef1a9;
+}
+	</style>
 </head>
 <body>
 <script src="//cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js"></script>
